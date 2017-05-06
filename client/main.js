032ed51cffcd4724dbd13b26f6d2d73a9414c48f
DcWind <danielekwuazi@gmail.com>
2017-05-06 21:21:02 +0700

    React added

--- client/main.js
+++ client/main.js
@@ -1,22 +1,16 @@
-import { Template } from 'meteor/templating';
-import { ReactiveVar } from 'meteor/reactive-var';
+import {React, Component} from 'react';
+import {Meteor} from 'meteor/meteor';
+import {render} from 'react-dom';
 
-import './main.html';
 
-Template.hello.onCreated(function helloOnCreated() {
-  // counter starts at 0
-  this.counter = new ReactiveVar(0);
+Meteor.startup(() => {
+  return render(<App />, document.getElementById('App'));
 });
 
-Template.hello.helpers({
-  counter() {
-    return Template.instance().counter.get();
-  },
-});
+class App extends Component {
 
-Template.hello.events({
-  'click button'(event, instance) {
-    // increment the counter when button is clicked
-    instance.counter.set(instance.counter.get() + 1);
-  },
-});
+  render(){
+    return <h1>React Integration test run </h1>;
+  }
+
+}
