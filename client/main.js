fe7708fd4b8f9d2d1de16b14b242d02e79eb47ec
DcWind <danielekwuazi@gmail.com>
2017-05-07 00:13:22 +0700

    imported App to main.js

--- client/main.js
+++ client/main.js
@@ -2,16 +2,10 @@ import React, { Component } from 'react';
 import { Meteor } from 'meteor/meteor';
 import { render } from 'react-dom';
 
+import App from '../imports/client/App';
 
 Meteor.startup(() => {
   console.log("start up");
   render(<App />, document.getElementById('App'));
 });
 
-class App extends Component {
-
-  render(){
-    return <h1>React Integration test run </h1>;
-  }
-
-}
