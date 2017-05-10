d3b089daeca28b800641f6d3e5ae3835e639b070
DcWind <danielekwuazi@gmail.com>
2017-05-10 23:59:09 +0700

    added Header to App

--- imports/client/App.js
+++ imports/client/App.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
+import Header from '../imports/client/Header/Header'
 
 export default class App extends Component {
 
   render() {
     return (
-    <h1>React Integration test run</h1>
+    <div>
+      <Header />
+      <h1>React Integration test run</h1>
+    </div>
+    
     )
   }
 
