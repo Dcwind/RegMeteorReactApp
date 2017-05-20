5c88555e976cc938323aa01ad6ef6962cddaaa0d
decent <decent@localhost.localdomain>
2017-05-20 20:49:21 +0700

    nav

--- imports/client/Header/Header.js
+++ imports/client/Header/Header.js
@@ -9,8 +9,8 @@ export default class Header extends Component {
       <a href="#" class="brand-logo">Logo</a>
       <ul id="nav-mobile" class="right hide-on-med-and-down">
         <li><a href="#">Sass</a></li>
-        <li><a href="#">Components</a></li>
-        <li><a href="#">JavaScript</a></li>
+        <li><a href="#">Register</a></li>
+        <li><a href="#">Login</a></li>
       </ul>
     </div>
   </nav>
